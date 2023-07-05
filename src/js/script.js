49a294641f3f7efb011d05b116f49cc9f6f27b84
AzzyPog <renato.longo1406@gmail.com>
2023-07-05 20:40:37 -0300

    feature: todos os comandos funcionando normalmente

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,24 @@ const playerOptions = document.querySelectorAll("#playerOptions img");
 var enemyOption = "";
 var playerOption = "";
 
+playerOptions.forEach((option) => {
+    option.addEventListener("click", () => {
+        playerOption = option.getAttribute("opt");
+        enemyChoice();
+    })
+});
+
+
+function enemyChoice() {
+    let randChoice = Math.round(Math.random() * 3)
+
+    const enemyOptions = document.querySelectorAll("#enemyOptions img");
+    const enemyChoice = enemyOptions[randChoice];
+    enemyOption = enemyChoice.getAttribute("opt");
+
+    validateGame();
+};
+
 
 function validateGame() {
     let vencedor = document.querySelector('#winner');
@@ -10,34 +28,35 @@ function validateGame() {
     
     
     if (playerOption == "papel") {
-        playerChoice.src = "./src/img/papel.png"
-        playerChoice.classList.remove("image-grow-animation");
+        playerChoice.src = "./src/img/papel.png";
+        playerChoice.classList.remove("image-grow-animation", "black-white");
+        enemyChoice.classList.remove("black-white");
+        vencedor.classList.remove("lose", "draw");
 
         if (enemyOption == "papel") {
             setTimeout(() => {
                 playerChoice.classList.add("image-grow-animation");
             }, 60);
-            enemyChoice.src = "./src/img/papel.png"
-            vencedor.innerHTML = "O jogo foi empatado";
+            enemyChoice.src = "./src/img/papel.png";
+            vencedor.innerHTML = "O jogo empatou";
             vencedor.classList.add("draw")
-            vencedor.classList.remove("lose")
-
+            
         } else if (enemyOption == "tesoura") {
             setTimeout(() => {
-                playerChoice.classList.add("image-grow-animation");
+                playerChoice.classList.add("image-grow-animation")
             }, 1);
-            enemyChoice.src = "./src/img/tesoura.png"
+            playerChoice.classList.add("black-white");
+            enemyChoice.src = "./src/img/tesoura.png";
             vencedor.innerHTML = "O inimigo ganhou";
-            vencedor.classList.add("lose")
-            vencedor.classList.remove("draw")
+            vencedor.classList.add("lose");
 
         } else if (enemyOption == "pedra") {
             setTimeout(() => {
                 playerChoice.classList.add("image-grow-animation");
             }, 60);
-            enemyChoice.src = "./src/img/pedra.png"
+            enemyChoice.classList.add("black-white");
+            enemyChoice.src = "./src/img/pedra.png";
             vencedor.innerHTML = "O player ganhou";
-            vencedor.classList.remove("draw", "lose")
         };
 
 
@@ -45,58 +64,68 @@ function validateGame() {
 
 
     if (playerOption == "tesoura") {
+        playerChoice.src = "./src/img/tesoura.png";
+        playerChoice.classList.remove("image-grow-animation", "black-white");
+        enemyChoice.classList.remove("black-white");
+        vencedor.classList.remove("lose", "draw");
 
         if (enemyOption == "papel") {
-            vencedor.innerHTML = "O jogo foi empatado";
-            vencedor.classList.add("draw");
-            vencedor.classList.remove("lose");
+            setTimeout(() => {
+                playerChoice.classList.add("image-grow-animation");
+            }, 60);
+            enemyChoice.src = "./src/img/papel.png";
+            vencedor.innerHTML = "O player ganhou";
+            enemyChoice.classList.add("black-white");
 
         } else if (enemyOption == "tesoura") {
-            vencedor.innerHTML = "O inimigo ganhou";
-            vencedor.classList.add("lose");
-            vencedor.classList.remove("draw");
+            setTimeout(() => {
+                playerChoice.classList.add("image-grow-animation")
+            }, 1);
+            enemyChoice.src = "./src/img/tesoura.png"
+            vencedor.innerHTML = "O jogo empatou";
+            vencedor.classList.add("draw")
 
         } else if (enemyOption == "pedra") {
-            vencedor.innerHTML = "O player ganhou";
-            vencedor.classList.remove("draw", "lose");
+            setTimeout(() => {
+                playerChoice.classList.add("image-grow-animation");
+            }, 60);
+            playerChoice.classList.add("black-white")
+            enemyChoice.src = "./src/img/pedra.png"
+            vencedor.innerHTML = "O inimigo ganhou";
         };
 
     };
 
     if (playerOption == "pedra") {
+        playerChoice.src = "./src/img/pedra.png";
+        playerChoice.classList.remove("image-grow-animation", "black-white");
+        enemyChoice.classList.remove("black-white");
+        vencedor.classList.remove("lose", "draw");
 
         if (enemyOption == "papel") {
-            vencedor.innerHTML = "O jogo foi empatado";
-            vencedor.classList.add("draw")
-            vencedor.classList.remove("lose")
+            setTimeout(() => {
+                playerChoice.classList.add("image-grow-animation");
+            }, 60);
+            enemyChoice.src = "./src/img/papel.png";
+            vencedor.innerHTML = "O inimigo ganhou";
+            playerChoice.classList.add("black-white");
 
         } else if (enemyOption == "tesoura") {
-            vencedor.innerHTML = "O inimigo ganhou";
-            vencedor.classList.add("lose")
-            vencedor.classList.remove("draw")
+            setTimeout(() => {
+                playerChoice.classList.add("image-grow-animation")
+            }, 1);
+            enemyChoice.src = "./src/img/tesoura.png";
+            enemyChoice.classList.add("black-white");
+            vencedor.innerHTML = "O player ganhou";
 
         } else if (enemyOption == "pedra") {
-            vencedor.innerHTML = "O player ganhou";
-            vencedor.classList.remove("draw", "lose")
+            setTimeout(() => {
+                playerChoice.classList.add("image-grow-animation");
+            }, 60);
+            enemyChoice.src = "./src/img/pedra.png"
+            vencedor.innerHTML = "O jogo empatou";
         };
 
     };
 
 };
-
-function enemyChoice() {
-    let randChoice = Math.round(Math.random() * 3)
-
-    const enemyOptions = document.querySelectorAll("#enemyOptions img")
-    const enemyChoice = enemyOptions[randChoice]
-    enemyOption = enemyChoice.getAttribute("opt")
-
-    validateGame()
-}
-
-playerOptions.forEach((option) => {
-    option.addEventListener("click", (t) => {
-        playerOption = option.getAttribute("opt")
-        enemyChoice()
-    })
-})
