d9d4c937fb19c7f37b35527e4cdf41bb3b85458e
AzzyPog <renato.longo1406@gmail.com>
2023-07-05 20:57:39 -0300

    feature: pequena melhora visual do código

--- src/js/script.js
+++ src/js/script.js
@@ -25,107 +25,42 @@ function validateGame() {
     let vencedor = document.querySelector('#winner');
     let playerChoice = document.querySelector("#playerChoice img");
     let enemyChoice = document.querySelector("#enemyChoice img");
-    
-    
-    if (playerOption == "papel") {
-        playerChoice.src = "./src/img/papel.png";
-        playerChoice.classList.remove("image-grow-animation", "black-white");
-        enemyChoice.classList.remove("black-white");
-        vencedor.classList.remove("lose", "draw");
 
-        if (enemyOption == "papel") {
-            setTimeout(() => {
-                playerChoice.classList.add("image-grow-animation");
-            }, 60);
-            enemyChoice.src = "./src/img/papel.png";
-            vencedor.innerHTML = "O jogo empatou";
-            vencedor.classList.add("draw")
-            
-        } else if (enemyOption == "tesoura") {
-            setTimeout(() => {
-                playerChoice.classList.add("image-grow-animation")
-            }, 1);
-            playerChoice.classList.add("black-white");
-            enemyChoice.src = "./src/img/tesoura.png";
-            vencedor.innerHTML = "O inimigo ganhou";
-            vencedor.classList.add("lose");
+    //reset das configurações
+    playerChoice.src = `./src/img/${playerOption}.png`;
+    playerChoice.classList.remove("image-grow-animation", "black-white");
+    enemyChoice.classList.remove("black-white");
+    vencedor.classList.remove("lose", "draw");
 
-        } else if (enemyOption == "pedra") {
-            setTimeout(() => {
-                playerChoice.classList.add("image-grow-animation");
-            }, 60);
-            enemyChoice.classList.add("black-white");
-            enemyChoice.src = "./src/img/pedra.png";
-            vencedor.innerHTML = "O player ganhou";
-        };
+    //se empate
+    if (playerOption == enemyOption) {
+        setTimeout(() => {
+            playerChoice.classList.add("image-grow-animation");
+        }, 1);
+        enemyChoice.src = `./src/img/${enemyOption}.png`;
+        vencedor.innerHTML = "O jogo empatou";
+        vencedor.classList.add("draw");
 
+    //se derrota do jogador    
+    } else if ((playerOption == "papel" && enemyOption == "tesoura") ||
+        (playerOption == "tesoura" && enemyOption == "pedra") ||
+        (playerOption == "pedra" && enemyOption == "papel")) {
 
-    };
+        setTimeout(() => {
+            playerChoice.classList.add("image-grow-animation");
+        }, 1);
+        playerChoice.classList.add("black-white");
+        enemyChoice.src = `./src/img/${enemyOption}.png`;
+        vencedor.innerHTML = "O inimigo ganhou";
+        vencedor.classList.add("lose");
 
-
-    if (playerOption == "tesoura") {
-        playerChoice.src = "./src/img/tesoura.png";
-        playerChoice.classList.remove("image-grow-animation", "black-white");
-        enemyChoice.classList.remove("black-white");
-        vencedor.classList.remove("lose", "draw");
-
-        if (enemyOption == "papel") {
-            setTimeout(() => {
-                playerChoice.classList.add("image-grow-animation");
-            }, 60);
-            enemyChoice.src = "./src/img/papel.png";
-            vencedor.innerHTML = "O player ganhou";
-            enemyChoice.classList.add("black-white");
-
-        } else if (enemyOption == "tesoura") {
-            setTimeout(() => {
-                playerChoice.classList.add("image-grow-animation")
-            }, 1);
-            enemyChoice.src = "./src/img/tesoura.png"
-            vencedor.innerHTML = "O jogo empatou";
-            vencedor.classList.add("draw")
-
-        } else if (enemyOption == "pedra") {
-            setTimeout(() => {
-                playerChoice.classList.add("image-grow-animation");
-            }, 60);
-            playerChoice.classList.add("black-white")
-            enemyChoice.src = "./src/img/pedra.png"
-            vencedor.innerHTML = "O inimigo ganhou";
-        };
-
-    };
-
-    if (playerOption == "pedra") {
-        playerChoice.src = "./src/img/pedra.png";
-        playerChoice.classList.remove("image-grow-animation", "black-white");
-        enemyChoice.classList.remove("black-white");
-        vencedor.classList.remove("lose", "draw");
-
-        if (enemyOption == "papel") {
-            setTimeout(() => {
-                playerChoice.classList.add("image-grow-animation");
-            }, 60);
-            enemyChoice.src = "./src/img/papel.png";
-            vencedor.innerHTML = "O inimigo ganhou";
-            playerChoice.classList.add("black-white");
-
-        } else if (enemyOption == "tesoura") {
-            setTimeout(() => {
-                playerChoice.classList.add("image-grow-animation")
-            }, 1);
-            enemyChoice.src = "./src/img/tesoura.png";
-            enemyChoice.classList.add("black-white");
-            vencedor.innerHTML = "O player ganhou";
-
-        } else if (enemyOption == "pedra") {
-            setTimeout(() => {
-                playerChoice.classList.add("image-grow-animation");
-            }, 60);
-            enemyChoice.src = "./src/img/pedra.png"
-            vencedor.innerHTML = "O jogo empatou";
-        };
-
-    };
-
-};
+    //se vitória do jogador    
+    } else {
+        setTimeout(() => {
+            playerChoice.classList.add("image-grow-animation");
+        }, 1);
+        enemyChoice.classList.add("black-white");
+        enemyChoice.src = `./src/img/${enemyOption}.png`;
+        vencedor.innerHTML = "O player ganhou";
+    }
+}
